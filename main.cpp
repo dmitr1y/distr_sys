f32aad7ff224cbcffc5476af2c97df2db17038da
Dmitriy Konstatninov <dmitriy@konstantinov.com.ru>
2018-05-08 10:56:26 +0300

    added mutex test

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
-#include <zconf.h>
+
 #include <vector>
+#include <mutex>
+#include <random>
 #include "MCS/mcs_lock.h"
 
 using mySync::mcs_lock;
-static int N_THREAD = 5000;
+static int N_THREAD = 128;
 static int N_REPEATS = 10;
+static int N_OPERATIONS = 100;
 
 void func(int &counter, mcs_lock &mcsLock) {
-    mcsLock.lock();
-    counter++;
-    mcsLock.unlock();
+    for (int i = 0; i < N_OPERATIONS; i++) {
+        mcsLock.lock();
+        counter++;
+        mcsLock.unlock();
+    }
+}
+
+void funcMutex(int &counter, std::mutex &mutexLock) {
+    for (int i = 0; i < N_OPERATIONS; i++) {
+        mutexLock.lock();
+        counter++;
+        mutexLock.unlock();
+
+    }
 }
 
 int main() {
-    long sum = 0;
+    long sum = 0, sumNS = 0;
     using Time = std::chrono::high_resolution_clock;
     typedef std::chrono::milliseconds ms;
     typedef std::chrono::duration<float> fsec;
@@ -35,11 +49,12 @@ int main() {
         fsec fs = t1 - t0;
         ms d = std::chrono::duration_cast<ms>(fs);
         sum += d.count();
-
-        if (counter != N_THREAD)
-            return -1;
+        sumNS += std::chrono::duration_cast<std::chrono::nanoseconds>(fs).count();
+//        if (counter != N_THREAD)
+//            return -1;
     }
-    std::cout << std::endl << "average time:  " << sum / N_REPEATS << " ms for " << N_THREAD << " threads" << std::endl;
+    std::cout << std::endl << "average time:  " << sum / N_REPEATS << " ms ( " << sumNS / N_REPEATS << " ns)" << " for "
+              << N_THREAD << " threads" << std::endl;
 
     return 0;
 }
